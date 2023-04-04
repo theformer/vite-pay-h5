dfc975e3e94dd0c7c251aa38fc2ee4edde34ba20
theformer <1020297391@qq.com>
2023-04-04 10:02:57 +0800

    变更

--- src/components/Layout/Tabbar.vue
+++ src/components/Layout/Tabbar.vue
@@ -1,16 +1,18 @@
 <template>
-  <van-tabbar v-model="active" route active-color="#0125B1">
+  <van-tabbar v-model="active" active-color="#0125B1">
     <van-tabbar-item>
       <span>返回顶部</span>
     </van-tabbar-item>
     <van-tabbar-item icon="home-o" class="item-buy">
-      <span>已购买</span>
       <template #icon="props">
-        <img
-          style="width: 19px; height: 19px; margin-top: 3px"
-          :src="props.active ? icon.classifyActive : icon.classifyInactive"
-          alt="classify"
-        />
+        <div class="buy-btn">
+          <img
+            style="width: 30px; height: 29px; margin-top: 3px"
+            :src="icon.classifyInactive"
+            alt="classify"
+          />
+          <span style="margin-left: 10px; color: red; font-weight: bolder">已购买</span>
+        </div>
       </template>
     </van-tabbar-item>
     <van-tabbar-item>
@@ -22,8 +24,7 @@
 import { ref } from 'vue'
 import { getAssetsImageUrl } from '@/utils'
 const icon = {
-  classifyInactive: getAssetsImageUrl('classify-inactive.png', 'icon'),
-  classifyActive: getAssetsImageUrl('classify-active.png', 'icon')
+  classifyInactive: getAssetsImageUrl('money.png', 'icon')
 }
 const active = ref(0)
 </script>
@@ -32,6 +33,15 @@ const active = ref(0)
 .van-tabbar {
   --van-tabbar-item-icon-margin-bottom: 5px;
   --van-tabbar-height: 60px;
+  .buy-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 125px;
+    background: #e2f920;
+    border-radius: 25%;
+    height: 120%;
+  }
   img {
     height: 22px;
   }
